fb33d000c4907d7324c305c000fd50bf99235d3b
Christopher2212 <chrisgx21@gmail.com>
2022-12-10 17:37:57 -0800

    tut removed

--- Web/__init__.py
+++ Web/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    
     from .pages import pages
 
     app.register_blueprint(pages, url_prefix = '/')
--- Web/pages.py
+++ Web/pages.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, render_template, request, redirect 
 import os 
+
 pages = Blueprint('pages', __name__)
 
 @pages.route("/", methods = ["GET","POST"])
 def home():
+    if request.method == "POST":
+        if request.files:
+
+            csv = request.files["csv"]
+            csv.save(os.path.join("uploads", csv.filename))
+            return redirect(request.url)
+            
     return render_template("index.html")
