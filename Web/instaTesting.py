87480dc7cfe65d3c93b46f24f72b8caa4446a65a
Christopher2212 <chrisgx21@gmail.com>
2022-12-11 18:25:28 -0800

    L

--- Web/instaTesting.py
+++ Web/instaTesting.py
@@ -1,5 +1,6 @@
 from instaloader import *  
-#import pandas as pd 
+import pandas as pd 
+from IPython.display import display
 user = "chrisgx21"
 
 L = instaloader.Instaloader() 
@@ -7,7 +8,13 @@ L.load_session_from_file(user)
 
 posts = Profile.from_username(L.context, 'chrisgx21').get_posts()
 
+rngDf = pd.DataFrame()
 for post in posts:
     for comment in post.get_comments(): 
-        print(comment)
-        
+        text = getattr(comment,"text")
+        id = getattr(comment,"id")
+        tempdf = {"ID": id,"Text": text}
+
+        rngDf = rngDf.append(tempdf, ignore_index = True)
+
+display(rngDf)
