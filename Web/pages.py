277fd27e2fa9453a79a7a1bd7a966498a3e93fad
Christopher2212 <chrisgx21@gmail.com>
2022-12-11 18:25:14 -0800

    wring to csv

--- Web/pages.py
+++ Web/pages.py
@@ -1,32 +1,46 @@
-from flask import Blueprint, render_template, request, redirect 
+from flask import Blueprint, render_template, request, redirect, url_for, session
 import os 
 import getFirefoxSession
-from instaloader import *  
+from instaloader import *
+import pandas as pd  
+
 pages = Blueprint('pages', __name__)
 
 @pages.route("/", methods = ["GET","POST"])
-def home():
-    if request.method == "POST":
-        if request.files:
+def index():
+    #If the user has submitted their username, send them over to other page 
+    if request.method == "POST": 
+        return redirect(url_for('pages.showBaseData'))
+    else: 
+        return render_template("index.html")
 
-            csv = request.files["csv"]
-            csv.save(os.path.join("uploads", csv.filename))
-            return redirect(request.url)
-            
-    return render_template("index.html")
 
-@pages.route("/", methods = ["GET", "POST"])
-def showBaseData(): 
-    if request.method == "POST":
-        # Login to the user's profile
-        username = request.form.get('username')
+@pages.route("/showBaseData", methods = ["GET", "POST"])
+def showBaseData():
+    #Checking to see if recieved post data
+    if request.method == "POST": 
+        usr = request.form["username"]
+
+        #Load session from Firefox Cookies 
         getFirefoxSession.runFireScript()
         L = instaloader.Instaloader() 
-        L.load_session_from_file(username)
-        posts = Profile.from_username(L.context, 'chrisgx21').get_posts()
 
+        #Logging in via session data
+        L.load_session_from_file(usr)
+
+        #getting posts from profile 
+        posts = Profile.from_username(L.context, usr).get_posts()
+        
+        #Getting all posts on Account and then going through each comment on post, grabbing text and id 
+        postDf = pd.DataFrame()
         for post in posts:
             for comment in post.get_comments(): 
-                print(comment)
+                text = getattr(comment,"text")
+                id = getattr(comment,"id")
 
-    return render_template("showBaseData.html")
+                tempdf = {"ID": id,"Text": text}
+                postDf = postDf.append(tempdf, ignore_index = True)
+        postDf.to_csv("Web\postData.csv")        
+        return render_template("showBaseData.html") 
+    else:
+        return f"<h1>Oh no! Something went wrong!<h1>"
